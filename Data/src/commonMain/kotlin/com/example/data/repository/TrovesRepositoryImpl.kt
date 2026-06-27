1294573b8bcba9be3f7d28db32ac63acd4f9083c
wahid <wahidqandill@gmail.com>
2026-06-27 15:53:04 +0300

    Add AuthenticationRepository.kt and its implementation Add TrovesRepository.kt.kt and its implementation Add TrovesApiService.kt.kt Add PaymentRepository.kt.kt and its implementation

---
+++ Data/src/commonMain/kotlin/com/example/data/repository/TrovesRepositoryImpl.kt
@@ -0,0 +1,9 @@
+package com.example.data.repository
+
+import com.example.data.source.remote.RemoteDatasourceImpl
+
+
+class TrovesRepositoryImpl(
+    private val remoteDatasourceImpl: RemoteDatasourceImpl
+) {
+}
