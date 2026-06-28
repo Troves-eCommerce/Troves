685b8c2810a983337692568833f43f2e6a4f28c1
yasse <yassenyasooo50@gmail.com>
2026-06-28 23:08:30 +0300

    refactor presentation module: update ViewModel instantiation to use viewModelOf

--- presintation/src/commonMain/kotlin/com/troves/presintation/di/PresentationModule.kt
+++ presintation/src/commonMain/kotlin/com/troves/presintation/di/PresentationModule.kt
@@ -2,11 +2,10 @@ package com.troves.presintation.di
 
 import com.troves.presintation.ui.products.ProductsViewModel
 import com.troves.presintation.ui.home.HomeViewModel
-import org.koin.compose.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val presentationModule = module {
-    // ViewModels — koin-compose-viewmodel handles lifecycle automatically
-    viewModel { ProductsViewModel(get()) }
-    viewModel { HomeViewModel(get(), get(), get(), get(), get()) }
+    viewModelOf(::ProductsViewModel)
+    viewModelOf(::HomeViewModel)
 }
