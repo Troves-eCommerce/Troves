d9b1ea4834f2a4db553ab4ad7866c7a17b5a43d9
esraaehab333 <esraaehabb333@gmail.com>
2026-06-29 15:46:10 +0300

    add app icon iOS & Android

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -123,15 +123,15 @@ buildkonfig {
     defaultConfigs {
         buildConfigField(
             STRING, "SHOPIFY_API_KEY",
-            localProperties.getProperty("SHOPIFY_API_KEY") ?: error("SHOPIFY_API_KEY not set in local.properties")
+            localProperties.getProperty("SHOPIFY_API_KEY") ?: ""
         )
         buildConfigField(
             STRING, "SHOPIFY_PASSWORD",
-            localProperties.getProperty("SHOPIFY_PASSWORD") ?: error("SHOPIFY_PASSWORD not set in local.properties")
+            localProperties.getProperty("SHOPIFY_PASSWORD") ?: ""
         )
         buildConfigField(
             STRING, "SHOPIFY_HOSTNAME",
-            localProperties.getProperty("SHOPIFY_HOSTNAME") ?: error("SHOPIFY_HOSTNAME not set in local.properties")
+            localProperties.getProperty("SHOPIFY_HOSTNAME") ?: ""
         )
     }
 }
