e18613257a846720f55c434c2eb174535912451a
esraaehab333 <esraaehabb333@gmail.com>
2026-06-29 07:13:24 +0300

    apply the KoinContext and SpTheme

--- shared/src/commonMain/kotlin/com/troves/App.kt
+++ shared/src/commonMain/kotlin/com/troves/App.kt
@@ -1,14 +1,17 @@
 package com.troves
 
-import androidx.compose.material3.MaterialTheme
+import com.troves.designsystem.theme.SpTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.troves.presintation.navigation.AppNavHost
+import org.koin.compose.KoinContext
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        AppNavHost()
+    KoinContext {
+        SpTheme {
+            AppNavHost()
+        }
     }
 }
