3fb7b9009322bb79ba42ebda70fa42089e18d8c3
esraaehab333 <esraaehabb333@gmail.com>
2026-06-29 07:12:37 +0300

    update package name and add FavoriteScreen.ktand ProfileScreen.kt as empty files

--- presintation/src/commonMain/kotlin/com/troves/presintation/ui/auth/LoginScreen.kt
+++ presintation/src/commonMain/kotlin/com/troves/presintation/ui/auth/LoginScreen.kt
@@ -1,4 +1,4 @@
-package com.troves.presintation.ui.Auth
+package com.troves.presintation.ui.auth
 import androidx.compose.runtime.Composable
 
 @Composable
---
+++ presintation/src/commonMain/kotlin/com/troves/presintation/ui/fav/FavoriteScreen.kt
@@ -0,0 +1,9 @@
+package com.troves.presintation.ui.fav
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FavoriteScreen(){
+    Text("Favorite Screen")
+}
