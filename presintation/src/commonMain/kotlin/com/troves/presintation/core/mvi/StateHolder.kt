e5a29a0c7e64915019091c603d980b225228c2c0
yasse <yassenyasooo50@gmail.com>
2026-06-30 13:28:13 +0300

    Add core MVI architecture components

---
+++ presintation/src/commonMain/kotlin/com/troves/presintation/core/mvi/StateHolder.kt
@@ -0,0 +1,25 @@
+package com.troves.presintation.core.mvi
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+
+interface StateHolder<S> {
+    val state: StateFlow<S>
+
+    val currentState: S get() = state.value
+
+    fun updateState(transform: S.() -> S)
+}
+
+
+class DefaultStateHolder<S>(initialState: S) : StateHolder<S> {
+    private val _state = MutableStateFlow(initialState)
+    override val state: StateFlow<S> = _state.asStateFlow()
+
+    override fun updateState(transform: S.() -> S) {
+        _state.update(transform)
+    }
+}
