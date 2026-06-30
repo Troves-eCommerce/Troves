1a34b117c9ceffa95ddb4fe75b92f6ab77cb6290
yasse <yassenyasooo50@gmail.com>
2026-06-30 13:28:14 +0300

    Refactor home feature to use unified MVI Contract

--- presintation/src/commonMain/kotlin/com/troves/presintation/ui/home/HomeUiEffect.kt
+++
@@ -1,7 +0,0 @@
-package com.troves.presintation.ui.home
-
-sealed interface HomeEffect {
-    data class NavigateToProduct(val productId: String) : HomeEffect
-    data class ShowToast(val message: String) : HomeEffect
-    data object NavigateToRegister : HomeEffect
-}
