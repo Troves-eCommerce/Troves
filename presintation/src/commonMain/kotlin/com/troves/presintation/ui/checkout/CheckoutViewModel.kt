e835385babfb26b517a2b8e6ee721fe6b1b5dfbc
wahid <wahidqandill@gmail.com>
2026-07-03 09:52:36 +0300

    Add Shopify Checkout Sheet Kit integration and initial UI components
    This change introduces the core infrastructure for checkout processing, including:
    * Added Shopify Checkout Sheet Kit dependencies for Android and Swift.
    * Defined `Checkout` and `CheckoutEvent` interfaces in the presentation layer.
    * Implemented `AndroidCheckout` using Shopify's `CheckoutSheetKit` to handle checkout events.
    * Created `CheckoutViewModel` and base UI components for the checkout flow.
    * Configured Shopify storefront credentials in `buildConfigField`.
    * Added domain entities for checkout exceptions and events.

---
+++ presintation/src/commonMain/kotlin/com/troves/presintation/ui/checkout/CheckoutViewModel.kt
@@ -0,0 +1,42 @@
+package com.troves.presintation.ui.checkout
+
+import androidx.lifecycle.ViewModel
+import com.troves.domain.entity.CheckoutCompletedEvent
+
+/**
+ * Copyright (c) 2026 Wahid Ali Wahid Hussien.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Author: Wahid Ali Wahid Hussien
+ * Created: 03/07/2026
+ */
+class CheckoutViewModel() : ViewModel(), CheckoutEvent {
+
+
+
+
+    override fun onCheckoutCanceled() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCheckoutCompleted(checkoutCompletedEvent: CheckoutCompletedEvent) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCheckoutFailed(error: Exception) {
+        TODO("Not yet implemented")
+    }
+}
