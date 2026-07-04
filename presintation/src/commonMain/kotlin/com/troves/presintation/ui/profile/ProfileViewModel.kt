7bd9f0fbbe61fa6f39e2a1424d6ab4c0053fd9c5
yasse <yassenyasooo50@gmail.com>
2026-07-04 22:01:51 +0300

    integrate AI Chat entry point into profile screen

--- presintation/src/commonMain/kotlin/com/troves/presintation/ui/profile/ProfileViewModel.kt
+++ presintation/src/commonMain/kotlin/com/troves/presintation/ui/profile/ProfileViewModel.kt
@@ -41,6 +41,7 @@ class ProfileViewModel(
             is ProfileIntent.ManageAddressesClicked -> emitEffect(ProfileEffect.NavigateToAddresses)
             is ProfileIntent.OrderHistoryClicked -> emitEffect(ProfileEffect.NavigateToOrders)
             is ProfileIntent.PaymentMethodsClicked -> emitEffect(ProfileEffect.NavigateToPaymentMethods)
+            is ProfileIntent.AiAssistantClicked -> emitEffect(ProfileEffect.NavigateToAiAssistant)
             is ProfileIntent.LanguageClicked -> Unit
             is ProfileIntent.LanguageSelected -> selectLanguage(intent.language)
             is ProfileIntent.DarkModeChanged -> setDarkMode(intent.enabled)
