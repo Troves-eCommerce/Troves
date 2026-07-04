031340223163ae0179215d656322515cd6633d31
yasse <yassenyasooo50@gmail.com>
2026-07-04 04:36:20 +0300

    Add Compose UI tooling dependency for enhanced UI development

--- presintation/build.gradle.kts
+++ presintation/build.gradle.kts
@@ -102,6 +102,7 @@ kotlin {
 
                 //Shopify Checkout Android kit
                 implementation(libs.checkout.sheet.kit.android)
+                implementation(libs.compose.uiTooling)
             }
         }
 
@@ -114,3 +115,4 @@ kotlin {
         }
     }
 }
+
