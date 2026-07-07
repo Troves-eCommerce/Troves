59bdf03c6c7c6b0f832bb3a590fc4c7db0b86612
wahid <wahidqandill@gmail.com>
2026-07-07 23:08:05 +0300

    Implement reverse geocoding in LocationRepository and update location service provider

--- data/src/commonMain/kotlin/com/troves/data/repository/LocationRepositoryImpl.kt
+++ data/src/commonMain/kotlin/com/troves/data/repository/LocationRepositoryImpl.kt
@@ -1,8 +1,12 @@
 package com.troves.data.repository
 
+import com.troves.data.source.framework.location.datasource.FrameworkLocationDatasource
 import com.troves.data.source.remote.location.LocationDataSource
+import com.troves.domain.entity.LocationAddress
+import com.troves.domain.entity.LocationCoordinates
 import com.troves.domain.repository.LocationRepository
 import com.troves.domain.utils.Result
+import com.troves.domain.utils.getOrThrow
 import com.troves.domain.utils.map
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
@@ -10,6 +14,7 @@ import kotlinx.coroutines.withContext
 
 class LocationRepositoryImpl(
     private val locationDataSource: LocationDataSource,
+    private val frameworkLocationDatasource: FrameworkLocationDatasource,
     private val coroutineDispatcher: CoroutineDispatcher = Dispatchers.Default,
 ) : LocationRepository {
 
@@ -22,9 +27,22 @@ class LocationRepositoryImpl(
         }
     }
 
-    override suspend fun getCities(countryName: String): Result<List<String>> = withContext(coroutineDispatcher) {
-        locationDataSource.getCities(countryName).map { response ->
-            response.data.orEmpty().distinct().sorted()
+    override suspend fun getCities(countryName: String): Result<List<String>> =
+        withContext(coroutineDispatcher) {
+            locationDataSource.getCities(countryName).map { response ->
+                response.data.orEmpty().distinct().sorted()
+            }
         }
+
+    // Framework Location
+
+    override suspend fun reverseGeocode(coordinates: LocationCoordinates): LocationAddress {
+        return frameworkLocationDatasource
+            .reverseGeocode(
+                coordinates = com.troves.data.source.framework.location.service.LocationCoordinates(
+                    coordinates.lan,
+                    coordinates.lon
+                )
+            ).getOrThrow().address?.toDomain()?: error("Address not found")
     }
 }
