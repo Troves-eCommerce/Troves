63d7724af06c6a18502045bec4d68f553f0fce3e
wahid <wahidqandill@gmail.com>
2026-07-08 16:43:10 +0300

    Implement iOS location permissions and location service implementation

--- data/src/iosMain/kotlin/com/troves/data/source/framework/location/service/FrameworkLocationServiceImpl.kt
+++ data/src/iosMain/kotlin/com/troves/data/source/framework/location/service/FrameworkLocationServiceImpl.kt
@@ -1,9 +1,51 @@
 package com.troves.data.source.framework.location.service
 
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.useContents
+import kotlinx.coroutines.suspendCancellableCoroutine
+import platform.CoreLocation.CLLocation
+import platform.CoreLocation.CLLocationManager
+import platform.CoreLocation.CLLocationManagerDelegateProtocol
+import platform.Foundation.NSError
+import platform.darwin.NSObject
+import kotlin.coroutines.resume
 
-class FrameworkLocationServiceImpl: FrameworkLocationService{
+class FrameworkLocationServiceImpl: FrameworkLocationService {
 
-    override suspend fun getCurrentLocationCoordinates(): LocationCoordinates {
-        TODO("Not yet implemented")
+    @OptIn(ExperimentalForeignApi::class)
+    override suspend fun getCurrentLocationCoordinates(): LocationCoordinates = suspendCancellableCoroutine { continuation ->
+        val locationManager = CLLocationManager()
+        
+        val delegate = object : NSObject(), CLLocationManagerDelegateProtocol {
+            override fun locationManager(manager: CLLocationManager, didUpdateLocations: List<*>) {
+                val location = didUpdateLocations.lastOrNull() as? CLLocation
+                if (location != null) {
+                    val lat = location.coordinate.useContents { latitude }
+                    val lon = location.coordinate.useContents { longitude }
+                    manager.stopUpdatingLocation()
+                    manager.delegate = null
+                    if (continuation.isActive) {
+                        continuation.resume(LocationCoordinates(lan = lat, lon = lon))
+                    }
+                }
+            }
+
+            override fun locationManager(manager: CLLocationManager, didFailWithError: NSError) {
+                manager.stopUpdatingLocation()
+                manager.delegate = null
+                if (continuation.isActive) {
+                    continuation.resume(LocationCoordinates(0.0, 0.0))
+                }
+            }
+        }
+        
+        locationManager.delegate = delegate
+        locationManager.startUpdatingLocation()
+
+        continuation.invokeOnCancellation {
+            locationManager.stopUpdatingLocation()
+            locationManager.delegate = null
+            val keepDelegate = delegate
+        }
     }
 }
