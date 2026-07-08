29fa7fc59697df9d272cc91913bd4093efb545d0
yasse <yassenyasooo50@gmail.com>
2026-07-08 14:49:44 +0300

    fix Shopify token provisioning and authentication flow

--- data/src/androidMain/kotlin/com/troves/data/repository/AuthenticationRepositoryImpl.android.kt
+++ data/src/androidMain/kotlin/com/troves/data/repository/AuthenticationRepositoryImpl.android.kt
@@ -3,10 +3,12 @@ package com.troves.data.repository
 import com.troves.data.source.local.preferenceses.TrovesPreferences
 import com.troves.data.source.remote.RemoteDatasource
 import com.troves.data.source.remote.service.StorefrontApiService
+import com.troves.data.source.remote.service.admin.ShopifyAdminCustomerService
 
 actual fun createAuthenticationRepository(
     preferences: TrovesPreferences,
     storefront: StorefrontApiService,
     remoteDatasource: RemoteDatasource,
+    adminCustomer: ShopifyAdminCustomerService,
 ): PlatformAuthenticationRepository =
-    AuthenticationRepositoryFirebaseImpl(preferences, storefront, remoteDatasource)
+    AuthenticationRepositoryFirebaseImpl(preferences, storefront, remoteDatasource, adminCustomer)
--- data/src/iosMain/kotlin/com/troves/data/repository/AuthenticationRepositoryImpl.ios.kt
+++ data/src/iosMain/kotlin/com/troves/data/repository/AuthenticationRepositoryImpl.ios.kt
@@ -2,6 +2,7 @@ package com.troves.data.repository
 
 import com.troves.data.source.local.preferenceses.TrovesPreferences
 import com.troves.data.source.remote.service.StorefrontApiService
+import com.troves.data.source.remote.service.admin.ShopifyAdminCustomerService
 
 import com.troves.data.source.remote.RemoteDatasource
 
@@ -9,5 +10,6 @@ actual fun createAuthenticationRepository(
     preferences: TrovesPreferences,
     storefront: StorefrontApiService,
     remoteDatasource: RemoteDatasource,
+    adminCustomer: ShopifyAdminCustomerService,
 ): PlatformAuthenticationRepository =
-    AuthenticationRepositoryFirebaseImpl(preferences, storefront, remoteDatasource)
+    AuthenticationRepositoryFirebaseImpl(preferences, storefront, remoteDatasource, adminCustomer)
